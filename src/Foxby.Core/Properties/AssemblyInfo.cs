f209895c96faecfa409ac83cc407cb3c737e89cb
eskat0n <muyou.prj@gmail.com>
2013-05-15 13:58:50 +0600

    changes to assembly info

--- src/Foxby.Core/Properties/AssemblyInfo.cs
+++ src/Foxby.Core/Properties/AssemblyInfo.cs
@@ -2,6 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Foxby")]
+[assembly: AssemblyTitle("TabulaRasa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TabulaRasa")]
+[assembly: AssemblyCopyright("Copyright © 2011-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
 [assembly: Guid("f0697d3b-1eba-427c-ab0a-2f36f4cb6160")]
-[assembly: InternalsVisibleTo("Foxby.Core.Tests")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("TabulaRasa.Tests")]
